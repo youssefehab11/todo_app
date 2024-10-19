580c46af1e75214f4a1a9db68de029c9c94d80e2
youssef <ucifehab11@gmail.com>
2024-10-19 21:05:48 +0300

    +Shared preferences and cache theme mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_app/application/application.dart';
+import 'package:todo_app/core/shared_preferences/shared_prefernces.dart';
 import 'package:todo_app/provider/app_provider.dart';
 
-void main() {
+void main()async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await AppSharedPreferences.init();
   runApp(
     ChangeNotifierProvider(
       create: (context) => AppProvider(),
